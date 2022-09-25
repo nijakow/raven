5b8e8106af4658767976fd7f1445a31c2de5ccfa
nijakow <79372954+nijakow@users.noreply.github.com>
2022-09-25 23:48:17 +0200

    Add license information to raven/channel.h and raven/channel.c

--- src/raven/channel.c
+++ src/raven/channel.c
@@ -1,3 +1,10 @@
+/*
+ * This file is part of the Raven MUD.
+ * Copyright (c) 2022  Eric Nijakowski
+ *
+ * See README and LICENSE for further information.
+ */
+
 #include "../util/memory.h"
 
 #include "channel.h"
