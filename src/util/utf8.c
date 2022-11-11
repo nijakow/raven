40afab1cfd56e1187b23c5f05a96b04630d25d3a
nijakow <79372954+nijakow@users.noreply.github.com>
2022-11-11 21:54:25 +0100

    Implemented utf8_decode

--- src/util/utf8.c
+++ src/util/utf8.c
@@ -7,7 +7,25 @@
 
 #include "utf8.h"
 
-raven_rune_t utf8_decode(const char* str, size_t* len);
+raven_rune_t utf8_decode(const char* str, size_t* len) {
+    raven_rune_t rune = 0;
+
+    if (str[0] < 0x80) {
+        rune = str[0];
+        if (len) *len = 1;
+    } else if ((str[0] & 0xE0) == 0xC0) {
+        rune = (str[0] & 0x1F) << 6 | (str[1] & 0x3F);
+        if (len) *len = 2;
+    } else if ((str[0] & 0xF0) == 0xE0) {
+        rune = (str[0] & 0x0F) << 12 | (str[1] & 0x3F) << 6 | (str[2] & 0x3F);
+        if (len) *len = 3;
+    } else if ((str[0] & 0xF8) == 0xF0) {
+        rune = (str[0] & 0x07) << 18 | (str[1] & 0x3F) << 12 | (str[2] & 0x3F) << 6 | (str[3] & 0x3F);
+        if (len) *len = 4;
+    }
+
+    return rune;
+}
 
 size_t utf8_encode(raven_rune_t rune, char* str) {
     if (rune < 0x80) {
