d8865cbb240edb781560cb6929d4c393edb093a0
nijakow <79372954+nijakow@users.noreply.github.com>
2023-01-22 17:31:19 +0100

    Implemented usernames and passwords

--- src/runtime/core/objects/misc/user.c
+++ src/runtime/core/objects/misc/user.c
@@ -18,13 +18,14 @@ struct obj_info USER_INFO = {
     .stats = (stats_func) base_obj_stats
 };
 
-struct user* user_new(struct raven* raven) {
+struct user* user_new(struct raven* raven, const char* name) {
     struct user*  user;
 
     user = base_obj_new(raven_objects(raven), &USER_INFO, sizeof(struct user));
 
     if (user != NULL) {
-        // ...
+        user->name     = memory_strdup(name);
+        user->password = memory_strdup("");
     }
 
     return user;
@@ -35,5 +36,17 @@ void user_mark(struct gc* gc, struct user* user) {
 }
 
 void user_del(struct user* user) {
+    memory_free(user->name);
+    memory_free(user->password);
     base_obj_del(&user->_);
+}
+
+
+
+bool user_compare_name(struct user* user, const char* name) {
+    return strcmp(user->name, name) == 0;
+}
+
+bool user_compare_password(struct user* user, const char* password) {
+    return strcmp(user->password, password) == 0;
 }
--- src/runtime/core/objects/misc/user.h
+++ src/runtime/core/objects/misc/user.h
@@ -17,10 +17,16 @@
 
 struct user {
     struct base_obj  _;
+
+    char*            name;
+    char*            password;
 };
 
-struct user* user_new(struct raven* raven);
+struct user* user_new(struct raven* raven, const char* name);
 void user_mark(struct gc* gc, struct user* user);
 void user_del(struct user* user);
+
+bool user_compare_name(struct user* user, const char* name);
+bool user_compare_password(struct user* user, const char* password);
 
 #endif
