5c110c9ea91f2aef4e41cd580d49e8af15490a79
nijakow <79372954+nijakow@users.noreply.github.com>
2023-02-12 13:51:00 +0100

    Persistence is now saving an empty mapping

--- src/raven/persistence/persistence.c
+++ src/raven/persistence/persistence.c
@@ -5,6 +5,9 @@
  * See README and LICENSE for further information.
  */
 
+#include "../../runtime/core/objects/mapping.h"
+#include "../raven.h"
+
 #include "serializer/serializer.h"
 
 #include "persistence.h"
@@ -19,7 +22,18 @@ void persistence_destroy(struct persistence* persistence) {
 }
 
 
+static struct symbol* persistence_get_symbol(struct persistence* persistence, const char* name) {
+    return raven_find_symbol(persistence->raven, name);
+}
+
+
 static bool persistence_save_raven(struct persistence* persistence, struct serializer* serializer) {
+    struct mapping*  toplevel_mapping;
+
+    toplevel_mapping = mapping_new(persistence->raven);
+
+    serializer_write_ptr(serializer, toplevel_mapping);
+
     return false;
 }
 
